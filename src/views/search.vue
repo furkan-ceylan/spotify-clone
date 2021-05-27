b352f573b7098e6106453a8c66ddca7e90131c85
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-28 02:01:45 +0300

    search page added

---
+++ src/views/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search">
+    <div class="sidebar">
+      <Sidebar />
+    </div>
+    <div class="header"></div>
+    Search
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
+
+export default {
+  components: { Sidebar, Footer },
+}
+</script>
+
+<style scoped>
+.search {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+}
+
+.header {
+  width: 100%;
+  height: 64px;
+  position: sticky;
+  top: 0;
+  background-color: var(--darkest);
+}
+</style>
