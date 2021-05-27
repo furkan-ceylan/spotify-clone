e371ac66c81d576cd73df3e299a8c6f942e356e4
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-28 01:59:57 +0300

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+.footer {
+  height: 12vh;
+  background-color: var(--dark);
+  flex: none;
+}
+</style>
