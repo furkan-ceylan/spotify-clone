402f06262c562a935c5a48a46568fbab7118b8ae
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-28 04:08:20 +0300

    header edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,23 @@
     <div class="sidebar">
       <Sidebar />
     </div>
-    <div class="header"></div>
+    <div class="header">
+      <div class="arrows">
+        <button class="arrow">
+          <i class="material-icons">keyboard_arrow_left</i>
+        </button>
+        <button class="arrow">
+          <i class="material-icons">keyboard_arrow_right</i>
+        </button>
+      </div>
+      <div>
+        <div class="profile">
+          <img class="profile-photo" src="../assets/profilePhoto.png" />
+          <span class="profile-name">User Name</span>
+          <i class="material-icons">arrow_drop_down</i>
+        </div>
+      </div>
+    </div>
     <!-- <div class="content">asdss</div> -->
   </div>
 </template>
@@ -23,13 +39,54 @@ export default {
   height: 100%;
   display: flex;
   position: relative;
+  color: white;
 }
 
 .header {
   width: 100%;
-  height: 64px;
+  height: 72px;
   position: sticky;
   top: 0;
   background-color: #212121;
+  display: flex;
+  justify-content: space-between;
+}
+
+.arrows {
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+  margin-top: 0.75rem;
+}
+
+.arrow {
+  background-color: black;
+  border-radius: 50%;
+  width: 2.5rem;
+  height: 2.5rem;
+  color: white;
+  margin-left: 1.5rem;
+}
+
+.profile {
+  display: flex;
+  align-items: center;
+  background-color: black;
+  border-radius: 9999px;
+  padding: 0.5rem;
+  margin-right: 2rem;
+  margin-top: 0.75rem;
+}
+
+.profile-photo {
+  border-radius: 9999px;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+}
+
+.profile-name {
+  font-weight: bold;
+  margin-right: 1rem;
 }
 </style>
