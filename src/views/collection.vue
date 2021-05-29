18045ab095e92a271e30dc856781e6084f08f608
furkan ceylan <ceylan.furkan@outlook.com>
2021-05-29 03:38:14 +0300

    header edit

--- src/views/collection.vue
+++ src/views/collection.vue
@@ -3,8 +3,36 @@
     <div class="sidebar">
       <Sidebar />
     </div>
-    <div class="header"></div>
-    Collection
+    <div class="header">
+      <div class="arrows">
+        <button class="arrow">
+          <i class="material-icons">keyboard_arrow_left</i>
+        </button>
+        <button class="arrow">
+          <i class="material-icons">keyboard_arrow_right</i>
+        </button>
+      </div>
+      <div class="header-right">
+        <div class="profile">
+          <button @click="showDropDown = !showDropDown">
+            <img class="profile-photo" src="../assets/profilePhoto.png" />
+            <span class="profile-name">User Name</span>
+            <i class="material-icons" v-if="!showDropDown">
+              arrow_drop_down
+            </i>
+            <i class="material-icons" v-else>
+              arrow_drop_up
+            </i>
+          </button>
+        </div>
+        <div class="profile-dropdown" v-if="showDropDown">
+          <button class="profile-button">Account</button>
+          <button class="profile-button">Profile</button>
+          <button class="profile-button">Sign Out</button>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="content">asdss</div> -->
   </div>
 </template>
 
@@ -14,6 +42,11 @@ import Footer from '../components/Footer'
 
 export default {
   components: { Sidebar, Footer },
+  data() {
+    return {
+      showDropDown: false,
+    }
+  },
 }
 </script>
 
@@ -23,13 +56,112 @@ export default {
   height: 100%;
   display: flex;
   position: relative;
+  color: white;
 }
 
 .header {
   width: 100%;
-  height: 64px;
+  height: 72px;
   position: sticky;
   top: 0;
-  background-color: var(--darkest);
+  background-color: #212121;
+  display: flex;
+  justify-content: space-between;
+}
+
+.header-right {
+  position: relative;
+}
+
+.arrows {
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+  margin-top: 0.75rem;
+}
+
+.arrow {
+  background-color: black;
+  border-radius: 50%;
+  width: 2.5rem;
+  height: 2.5rem;
+  color: white;
+  margin-left: 1.5rem;
+}
+
+.profile {
+  display: flex;
+  align-items: center;
+  background-color: black;
+  border-radius: 9999px;
+  padding: 0.5rem;
+  margin-right: 2rem;
+  margin-top: 0.75rem;
+  cursor: pointer;
+}
+
+.profile button {
+  color: white;
+}
+
+.profile i {
+  position: absolute;
+  top: 1.25rem;
+  right: 1.75rem;
+}
+
+.profile:hover {
+  background-color: var(--light);
+}
+
+.profile-photo {
+  border-radius: 9999px;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+}
+
+.profile-name {
+  font-weight: bold;
+  margin-right: 1rem;
+}
+
+.profile-dropdown {
+  position: absolute;
+  background-color: var(--light);
+  width: 100%;
+  margin-top: 1rem;
+  right: 2rem;
+  border: 5px solid var(--light);
+}
+
+.profile-button {
+  width: 100%;
+  color: white;
+  font-weight: bold;
+  text-align: left;
+  padding-left: 1rem;
+  padding-bottom: 0.75rem;
+  padding-top: 0.75rem;
+}
+
+.profile-button:hover {
+  background-color: var(--gray);
+}
+
+@media only screen and (max-width: 1280px) {
+  .profile-name {
+    display: none;
+  }
+  .profile i {
+    display: none;
+  }
+  .profile-photo {
+    margin-right: 0;
+  }
+  .profile-dropdown {
+    width: 200px;
+    margin-top: 1rem;
+  }
 }
 </style>
